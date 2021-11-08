c4e77224c75615e36919e54307437806f0a71c9f
aleangelozi <aleangelozi@gmail.com>
2021-11-08 07:06:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spinner-codelab
+Source: https://www.youtube.com/watch?v=ovGZYK9bq2o&list=PLQkwcJG4YTCTq1raTb5iMuxnEB06J1VHX
---
+++ app/src/main/java/com/aleangelozi/spinnercodelab/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.aleangelozi.spinnercodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val adapter = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, customList)
+        spinner.adapter = adapter
+
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                    "You selected ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+    }
+}
